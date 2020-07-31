2ec6ea2bd5c67b35b2bb8c03ac8f766918717fca
James Peachey <James.Peachey@jhuapl.edu>
2020-07-31 13:48:50 -0400

    RM 2094: compute and cache rotation matrix for inst-to-center transform.

--- src/edu/jhuapl/sbmt/pointing/spice/SpiceInstrumentPointing.java
+++ src/edu/jhuapl/sbmt/pointing/spice/SpiceInstrumentPointing.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.ImmutableList;
+
 import edu.jhuapl.sbmt.pointing.AbstractInstrumentPointing;
 
+import crucible.core.math.vectorspace.RotationMatrixIJK;
 import crucible.core.math.vectorspace.UnwritableVectorIJK;
 import crucible.core.mechanics.Coverage;
 import crucible.core.mechanics.EphemerisID;
 import crucible.core.mechanics.FrameID;
+import crucible.core.mechanics.FrameTransformFunction;
 import crucible.core.mechanics.StateVector;
 import crucible.core.mechanics.StateVectorFunction;
 import crucible.core.mechanics.StateVectorFunctions;
@@ -32,6 +36,7 @@ public final class SpiceInstrumentPointing extends AbstractInstrumentPointing
     private final Map<EphemerisID, UnwritableStateVector> bodyStates;
     private UnwritableVectorIJK scPos;
     private double timeAtTarget;
+    private RotationMatrixIJK instToCenterRotation;
 
     public SpiceInstrumentPointing( //
             AberratedEphemerisProvider ephProvider, //
@@ -55,16 +60,11 @@ public final class SpiceInstrumentPointing extends AbstractInstrumentPointing
         this.frustum = frustum;
         this.time = time;
         this.bodyStates = new HashMap<>();
-        this.scPos = null;
-        this.timeAtTarget = -1.; // Just for debugging, so one can easily tell whether it has been computed yet.
+        this.scPos = null; // Not computed yet.
+        this.timeAtTarget = -1.; // Not computed yet.
+        this.instToCenterRotation = null; // Not computed yet.
     }
 
-    /**
-     * Return a vector that gives the spacecraft position relative to the body
-     * fixed frame.
-     *
-     * @return the spacecraft position vector
-     */
     public UnwritableVectorIJK getSpacecraftPos()
     {
         computeScPointing();
@@ -72,18 +72,13 @@ public final class SpiceInstrumentPointing extends AbstractInstrumentPointing
         return scPos;
     }
 
-    /**
-     * Return a vector that gives the sun position relative to the body fixed
-     * frame.
-     *
-     * @return the sun position vector
-     */
     public UnwritableVectorIJK getPos(EphemerisID bodyId)
     {
         UnwritableStateVector bodyState = bodyStates.get(bodyId);
         if (bodyState == null)
         {
-            computeScPointing(); // Needed so timeAtTarget is computed correctly.
+            // Needed to guarantee timeAtTarget is computed correctly.
+            computeScPointing();
 
             AberratedStateVectorFunction bodyFromTarget = ephProvider.createAberratedStateVectorFunction(bodyId, targetId, centerFrameId, Coverage.ALL_TIME, AberrationCorrection.LT_S);
             bodyState = UnwritableStateVector.copyOf(bodyFromTarget.getState(timeAtTarget));
@@ -94,68 +89,25 @@ public final class SpiceInstrumentPointing extends AbstractInstrumentPointing
         return bodyState.getPosition();
     }
 
-    /**
-     * Return a unit vector that indicates the direction of the instrument
-     * boresight axis.
-     *
-     * @return the boresight vector
-     */
     public UnwritableVectorIJK getBoresight()
     {
-        return boresight;
+        computeScPointing();
+
+        return normalize(instToCenterRotation.mxv(boresight));
     }
 
-    /**
-     * Return a unit vector that indicates the "up" direction of the instrument.
-     *
-     * @return the up vector
-     */
     public UnwritableVectorIJK getUp()
     {
-        return upDir;
+        computeScPointing();
+
+        return normalize(instToCenterRotation.mxv(upDir));
     }
 
-    /**
-     * Return an object that represents the frustum of the instrument.
-     *
-     * @return the frustum
-     */
     public List<UnwritableVectorIJK> getFrustum()
     {
-        return frustum;
-    }
+        computeScPointing();
 
-    /**
-     * Return a vector in the same direction as the input vector, but guaranteed
-     * unwritable, and normalized to length 1 (unit vector) if possible. This
-     * method is needed because similar methods of {@link UnwritableVectorIJK}
-     * have some undesirable corner-case behaviors.
-     * <p>
-     * Provided all components of the provided vector are finite, the result is
-     * guaranteed to be either a unit vector or a vector of length 0, with no
-     * exceptions thrown.
-     *
-     * @param vector the input vector
-     * @return a vector in the same direction as the input vector, but with
-     *         length equal to 1, or else with length equal to 0 if the input
-     *         vector has 0 length
-     */
-    public static UnwritableVectorIJK normalize(UnwritableVectorIJK vector)
-    {
-        // If the norm is anything other than 0 or 1, need to normalize the
-        // input.
-        double norm = vector.getLength();
-        boolean normalize = norm != 1. && norm != 0.;
-
-        // Make a copy in order to normalize or to ensure unwritable.
-        boolean copy = normalize || vector.getClass() != UnwritableVectorIJK.class;
-
-        if (copy)
-        {
-            vector = normalize ? new UnwritableVectorIJK(1.0 / norm, vector) : new UnwritableVectorIJK(vector);
-        }
-
-        return vector;
+        return rotateAll(frustum, instToCenterRotation);
     }
 
     private void computeScPointing()
@@ -171,10 +123,24 @@ public final class SpiceInstrumentPointing extends AbstractInstrumentPointing
             // Get position of body relative to spacecraft.
             StateVector bodyFromScState = scFromBody.getState(time);
 
-            scPos = UnwritableVectorIJK.copyOf(bodyFromScState.getPosition());
-
             timeAtTarget = time - bodyFromSc.getLightTime(time);
+
+            FrameTransformFunction instToCenter = ephProvider.createFrameTransformFunction(instFrame, centerFrameId, Coverage.ALL_TIME);
+
+            instToCenterRotation = instToCenter.getTransform(time);
+
+            scPos = UnwritableVectorIJK.copyOf(bodyFromScState.getPosition());
         }
     }
 
+    private ImmutableList<UnwritableVectorIJK> rotateAll(List<UnwritableVectorIJK> vectors, RotationMatrixIJK rotation)
+    {
+        ImmutableList.Builder<UnwritableVectorIJK> builder = ImmutableList.builder();
+        for (UnwritableVectorIJK vector : vectors)
+        {
+            builder.add(UnwritableVectorIJK.copyOf(rotation.mxv(vector)));
+        }
+
+        return builder.build();
+    }
 }
