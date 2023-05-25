7257e9104c92247b6eeec599f4a07a8e04e8a49e
carrbd1 <carrbd1@carrbd1-wl1.dom1.jhuapl.edu>
2023-05-25 09:51:12 -0400

    RM 2530: Fix for Windows-specific error upon loading SPICE kernel

--- src/edu/jhuapl/sbmt/pointing/spice/MetaKernelHelper.java
+++ src/edu/jhuapl/sbmt/pointing/spice/MetaKernelHelper.java
@@ -60,6 +60,7 @@ class MetaKernelHelper
             {
                 Pattern rp = pathSymbols.get(index);
                 String replaceString = pathValues.get(index).toString();
+                replaceString = replaceString.replaceAll("\\\\+", File.separator + File.separator + File.separator + File.separator);
                 localKernelFileName = rp.matcher(localKernelFileName).replaceAll(replaceString);
             }
             localKernels.add(new File(localKernelFileName));
